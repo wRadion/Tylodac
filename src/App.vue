537979c450c1b89f4955f614a2d05d2248e3f3c0
wRadion <wradion@gmail.com>
2020-08-20 14:00:49 +0200

    First commit (createapp.dev)

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div>
+    <h1>
+      {{name}}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    data: function() {
+      return {
+        name: 'Hello World!',
+      }
+    },
+  });
+</script>
+
+<style lang="scss">
+$primary-color: white;
+$bg: black;
+h1 {
+  color: $primary-color;
+  background-color: $bg;
+}
+</style>
