90fb383614b21d46c876092baae241c010a87ec5
wRadion <wradion@gmail.com>
2020-08-21 18:36:47 +0200

    Added login server/client socket logic

--- server.js
+++ server.js
@@ -2,8 +2,39 @@ const io = require('socket.io')();
 
 const port = 4321;
 
+var currentId = 0;
+var clients = [];
+
+function isUsernameTaken(username) {
+  return clients.findIndex(c => c.username === username) >= 0;
+}
+
 io.on('connection', (socket) => {
-  // do something
+  const index = clients.length;
+  const client = {
+    id: currentId++,
+    username: null
+  }
+  clients.push(client);
+  function log(message) { console.log('[client ' + client.id + '] ' + message); }
+
+  log('connected');
+
+  socket.on('disconnect', () => {
+    log('disconnected')
+  });
+
+  socket.on('client_login', (username) => {
+    if (isUsernameTaken(username)) return;
+    clients[index].username = username;
+    log('login');
+  });
+
+  socket.on('client_check_username', (username) => {
+    socket.emit('server_check_username_response', isUsernameTaken(username));
+    log('check_username');
+  });
+
 });
 
 console.log('listening on port ' + port + '...');
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="wrapper">
     <img src="/assets/logo.png" />
-    <form>
+    <form @submit.prevent="login">
       <label for="username">Username</label>
-      <input id="username" type="text" autocomplete="off" autofocus>
-      <input id="login" type="submit" v-on:click.prevent='login' value="Login">
+      <input id="username"
+             v-bind:class="{ error: this.hasError }"
+             v-model.trim="username"
+             type="text"
+             autocomplete="off"
+             autofocus>
+      <input id="login"
+             type="submit"
+             value="Login"
+             v-bind:disabled="!connected">
+      <div v-if="this.hasError" class="error-message">{{ this.error }}</div>
     </form>
   </div>
 </template>
@@ -12,8 +21,51 @@
 <script>
 export default {
   name: 'LoginView',
+  data: function() {
+    return {
+      username: '',
+      error: null,
+      connected: false
+    }
+  },
+  watch: {
+    username: function(newValue, oldValue) {
+      this.error = null;
+      this.checkUsername();
+    }
+  },
+  sockets: {
+    connect: function() {
+      this.connected = true;
+    },
+    connect_error: function(error) {
+      this.connected = false;
+      alert('Could not connect to the server. Please wait and refresh this page.');
+    },
+    server_check_username_response: function(isTaken) {
+      if (isTaken) {
+        this.error = 'This username is taken';
+      }
+    }
+  },
+  computed: {
+    hasError: function() { return !!this.error; }
+  },
   methods: {
-    login: function() { alert('test'); }
+    checkUsername: function() {
+      if (this.username.length < 3) {
+        this.error = 'Username must be at least 3 characters long';
+      }
+    },
+    validateUsername: function() {
+      this.checkUsername();
+      this.$socket.emit('client_check_username', this.username);
+    },
+    login: function() {
+      this.validateUsername();
+      if (this.hasError) return;
+      this.$socket.emit('client_login', this.username);
+    }
   }
 }
 </script>
